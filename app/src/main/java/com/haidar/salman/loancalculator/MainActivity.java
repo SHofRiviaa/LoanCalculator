f8e97e95f6aee5fd65487570f2ffbb283e35bc05
Salman Haidar <salmanhaidar9576@gmail.com>
2017-06-04 00:28:42 -0400

    clear button event added

--- app/src/main/java/com/haidar/salman/loancalculator/MainActivity.java
+++ app/src/main/java/com/haidar/salman/loancalculator/MainActivity.java
@@ -2,6 +2,11 @@ package com.haidar.salman.loancalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clear(View view){
+
+        //Clear loan amount
+        TextView tv = (EditText)findViewById(R.id.loanEdit);
+        tv.setText("");
+
+        //Clear term of loan
+        tv = (EditText)findViewById(R.id.termEdit);
+        tv.setText("");
+
+        //Clear yearly interest rate
+        tv = (EditText)findViewById(R.id.yearlyEdit);
+        tv.setText("");
+
+        //Clear monthly payment
+        tv = (TextView)findViewById(R.id.monthPayTxtAnswer);
+        tv.setText("766.91");
+
+        //Clear total payment
+        tv = (TextView)findViewById(R.id.totPaymAnswer);
+        tv.setText("64420.44");
+
+        //Clear total interest
+        tv = (TextView)findViewById(R.id.totInterestAnswer);
+        tv.setText("14420.44");
+    }
+
 }
